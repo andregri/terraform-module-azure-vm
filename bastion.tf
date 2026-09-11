9e2cd9818b0d10a0890c5b0d33bc6e6f8c8c845e
Andrea <andrea.grillo96@gmail.com>
2026-09-11 11:13:48 +0200

    feat: add bastion host to connect to vm

---
+++ bastion.tf
@@ -0,0 +1,30 @@
+# Create Subnet for Azure Bastion
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.3.0/26"]
+}
+
+# Create Public IP for Azure Bastion
+resource "azurerm_public_ip" "bastion_pip" {
+  name                = "${var.prefix}-bastion-pip"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Create Azure Bastion Host
+resource "azurerm_bastion_host" "bastion" {
+  name                = "${var.prefix}-network-bastion"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  sku                 = "Standard"
+
+  ip_configuration {
+    name                 = "IpConf"
+    subnet_id            = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion_pip.id
+  }
+}
