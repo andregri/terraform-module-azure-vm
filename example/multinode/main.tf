c9be362ceb8de9c3e1c21979326b48cedf184f68
Andrea <andrea.grillo96@live.com>
2026-09-14 16:20:49 +0200

    enable bastion host on multi-node example

--- example/multinode/main.tf
+++ example/multinode/main.tf
@@ -1,6 +1,7 @@
 module "multinode" {
   source = "../../"
 
+  enable_bastion      = true
   prefix              = "multinode"
   resource_group_name = "1-c95f1421-playground-sandbox"
   vm_count            = 4
