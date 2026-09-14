f4ff6af5d6ce54b5cd6cce8865f16acdcff19da4
Andrea <andrea.grillo96@live.com>
2026-09-14 15:33:42 +0200

    mv main.tf -> network.tf

--- vm.tf
+++ vm.tf
@@ -1,22 +1,3 @@
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
   location              = data.azurerm_resource_group.example.location
